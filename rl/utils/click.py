9f59d41b13b631a88fb200150e87b6fc5f558788
Faiz Surani <faiz.surani@gmail.com>
2024-06-29 14:36:34 -0700

    Show default by default in rl.utils.click

--- rl/utils/click.py
+++ rl/utils/click.py
@@ -28,6 +28,11 @@ def excepthook(type, value, tb):
 
 
 def command(*args, **kwargs):
+    context_settings = kwargs.get("context_settings", {})
+    if "show_default" not in context_settings:
+        context_settings["show_default"] = True
+    kwargs["context_settings"] = context_settings
+
     def decorator(f):
         sys.excepthook = excepthook
         return rich_click.command(*args, **kwargs)(f)
